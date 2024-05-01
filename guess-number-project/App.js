09ac483b903697e9f7459aae0568cb37e165daa7
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-05-01 10:22:27 +0000

    Coloring the components & The overall App

--- guess-number-project/App.js
+++ guess-number-project/App.js
@@ -3,7 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return <StartGameScreen />
+  return (
+    <View style={styles.rootScreen}>
+        <StartGameScreen />
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: "#ddb52f"
+  }
+});
