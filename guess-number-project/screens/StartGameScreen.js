04a51d81d8ba8bd29561c36488b0fc56da773e02
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-05-01 10:01:02 +0000

    Adding visual feedback to the buttons

--- guess-number-project/screens/StartGameScreen.js
+++ guess-number-project/screens/StartGameScreen.js
@@ -6,7 +6,12 @@ function StartGameScreen() {
 
     return ( 
             <View style={styles.inputContainer}>
-                <TextInput />
+                <TextInput style={styles.numberInput} 
+                           maxLength={2} 
+                           keyboardType="number-pad"
+                           autoCapitalize="none"
+                           autoCorrect={false}
+                 />
                 <PrimaryButton>Reset</PrimaryButton>
                 <PrimaryButton>Confirm</PrimaryButton>
             </View>
@@ -22,12 +27,24 @@ const styles = StyleSheet.create({
         marginTop: 100,
         padding: 16,
         // alignItems: "center",
-        backgroundColor: "#72063c",
+        backgroundColor: "#4e0329",
         marginHorizontal: 24,
         borderRadius: 8,
         elevation: 4,   // Android
         shadowColor: 'black', // iOS
         shadowOffset: {width: 0, height: 2}, // iOS,
         shadowOpacity: 0.5, // iOS,
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: "#ddb52f",
+        borderBottomWidth: 2,
+        color: "#ddb52f",
+        marginVertical: 8,
+        fontWeight: "bold",
+        textAlign: "center"
+
     }
 })
