ad50e175e13155ec2966ea457bfea0c0dcd27484
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-05-04 11:35:15 +0000

    Creating a title

--- guess-number-project/screens/GameScreen.js
+++ guess-number-project/screens/GameScreen.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
+import Title from "../components/title";
 
 
 function GameScreen() {
     return (
         <View style={styles.screen}>
-            <Text>Opponent's screen</Text>
+            <Title>Opponent's screen</Title>
             <Text>GUESS</Text>
             <View>
                 <Text>Higher or lower ?</Text>
@@ -20,6 +21,6 @@ export default GameScreen;
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        padding: 20
+        padding: 40,
     }
 })
