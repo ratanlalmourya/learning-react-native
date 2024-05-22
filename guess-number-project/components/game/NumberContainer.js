fb1bc4fb08dc9d2d54c9eaad0ee5c4c2cab3687e
rattumourya <160098742+rattumourya@users.noreply.github.com>
2024-05-22 05:50:00 +0000

    Adjusting Image Sizes with the dimensions API

--- guess-number-project/components/game/NumberContainer.js
+++ guess-number-project/components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 import Colors from "../../constants/colors";
 
 function NumberContainer({children}) {
@@ -9,21 +9,23 @@ function NumberContainer({children}) {
     )
 }
 
+const deviceWidth = Dimensions.get('window').width;
+
 export default NumberContainer;
 
 const styles = StyleSheet.create({
     container: {
         borderWidth: 4,
         borderColor: Colors.accent500,
-        padding: 24,
+        padding: deviceWidth < 450 ? 12 : 24,
         borderRadius: 8,
-        margin: 24,
+        margin: deviceWidth < 450 ? 12 : 24,
         alignItems: 'center',
         justifyContent: 'center'
     },
     numberText: {
         color: Colors.accent500,
-        fontSize: 36,
+        fontSize: deviceWidth < 380 ? 28 : 36,
         fontWeight: 'bold'
     }
 })
