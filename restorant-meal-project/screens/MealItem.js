30ba57029948e4486997dab6826cdd9a39a25d09
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-06-05 06:17:29 +0000

    Adding images & styling

--- restorant-meal-project/screens/MealItem.js
+++ restorant-meal-project/screens/MealItem.js
@@ -1,10 +1,65 @@
-import { Text, View } from "react-native";
+import { Image, Pressable, Text, View, StyleSheet, Platform } from "react-native";
 
-function MealItem({title}) {
+function MealItem({title, imageURL, duration, complexity, affordability }) {
 
-    return <View>
-        <Text>{title}</Text>
+    return <View style={styles.mealItem}>
+        <Pressable android_ripple={{color: '#ccc'}}
+                    style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+                   >
+            <View style={styles.innerContainer}>
+                    <View>
+                        <Image source={{uri : imageURL}} style={styles.image}  />
+                        <Text style={styles.title}>{title}</Text>
+                    </View>
+                    <View style={styles.details}>
+                        <Text style={styles.detailItem}>{duration}m</Text>
+                        <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+                        <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+                    </View>
+            </View>
+        </Pressable>
     </View>
 }
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200,
+    },
+    title: {
+        fontSize: 'bold',
+        textAlign: 'center',
+        fontSize: 18,
+        margin: 8
+    },
+    mealItem: {
+        margin: 16,
+        borderRadius: 8,
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+        backgroundColor: 'white',
+        elevation: 4,   
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width:  0, height: 2},
+        shadowRadius: 8,
+    },
+    details: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 8
+    },
+    detailItem: {
+        marginHorizontal: 4,
+        fontSize: 12
+    },
+    innerContainer: {
+        borderRadius: 8,
+        overflow: 'hidden'
+    },
+    buttonPressed: {
+        opacity: 0.5
+    }
+})
--- restorant-meal-project/screens/MealsOverviewScreen.js
+++ restorant-meal-project/screens/MealsOverviewScreen.js
@@ -12,7 +12,11 @@ function MealsOverviewScreen({ route }) {
 
 
     function renderMealItem(itemData) {
-        return <MealItem title={itemData.item.title} />
+        return <MealItem title={itemData.item.title} 
+                        imageURL={itemData.item.imageUrl}
+                        duration={itemData.item.duration}
+                        complexity={itemData.item.complexity}
+                        affordability={itemData.item.affordability} />
     }
 
     return (
