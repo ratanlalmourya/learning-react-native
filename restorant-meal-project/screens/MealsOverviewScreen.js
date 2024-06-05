8b5520057720e1eaa2849e81ec08f878405c2845
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-06-05 05:03:03 +0000

    Displaying meals

--- restorant-meal-project/screens/MealsOverviewScreen.js
+++ restorant-meal-project/screens/MealsOverviewScreen.js
@@ -1,15 +1,26 @@
 
+import { MEALS } from "../dummy-data";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
-import { View, Text, StyleSheet } from "react-native";
-import  {MEALS} from "../models/meal";
+import MealItem from "./MealItem";
 
 function MealsOverviewScreen({ route }) {
+    const catId = route.params.categoryId;
+    const displayedMeals = MEALS.filter((mealItem) => {
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    });
 
-    const catId = route.params.categoryId
+
+    function renderMealItem(itemData) {
+        return <MealItem title={itemData.item.title} />
+    }
 
     return (
         <View style={styles.container}>
-            <Text>Meals Overview Screen - {catId}</Text>
+           <FlatList    
+            data={displayedMeals}
+            keyExtractor={item => item.id}
+            renderItem={renderMealItem} />
         </View>
     )
 }
