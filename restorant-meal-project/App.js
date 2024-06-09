7abd7a7e0ca3e07763756536db062d2cdea5f679
ratanlalmourya <ratanlalmourya41@gmail.com>
2024-06-09 16:01:11 +0530

    Nesting Navigators

--- restorant-meal-project/App.js
+++ restorant-meal-project/App.js
@@ -2,13 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return <Drawer.Navigator 
+            screenOptions={{
+                  headerStyle: { backgroundColor: "#351401"},
+                  headerTintColor: "#FFF",
+                  sceneContainerStyle: {backgroundColor: "#3f2f25"} 
+      }}>
+    <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+     />
+    <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+    />
+  </Drawer.Navigator>
+}
 
 export default function App() {
   return (
@@ -21,10 +41,10 @@ export default function App() {
                           contentStyle: {backgroundColor: "#3f2f25"}
         }}>
             <Stack.Screen 
-              name="MealsCategories" 
-              component={CategoriesScreen}
+              name="Drawer" 
+              component={DrawerNavigator}
               options={{
-                title: "All Categories",
+                headerShown: false
               }}
             />
             <Stack.Screen name="MealsOverview" 
