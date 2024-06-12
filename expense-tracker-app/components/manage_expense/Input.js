39c155f54997749f7ddd132f6eef7aad73b3e6f2
ratanlalmourya <ratanlalmourya41@gmail.com>
2024-06-12 19:31:56 +0530

    Adding styles to expense form

--- expense-tracker-app/components/manage_expense/Input.js
+++ expense-tracker-app/components/manage_expense/Input.js
@@ -1,12 +1,42 @@
-import { TextInput, View, Text } from "react-native";
+import { TextInput, View, Text, StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
-function Input({label,textInputConfig}){
+function Input({label,textInputConfig}){    
+
+    let inputStyle = [styles.input];
+    if(textInputConfig && textInputConfig.multiline) {
+        inputStyle.push(styles.inputMultiLine)
+    }
     return (
-        <View>
-            <Text>{label}</Text>
-            <TextInput {...textInputConfig} />
+        <View style={styles.inputContainer}>
+            <Text style={styles.label}>{label}</Text>
+            <TextInput style={inputStyle} {...textInputConfig} />
         </View>
     )
 }
 
 export default Input;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginHorizontal: 4,
+        marginVertical: 8
+    },
+    label: {
+        fontSize: 12,
+        color: GlobalStyles.colors.primary100,
+        marginBottom: 4,
+
+    },
+    input: {
+        backgroundColor: GlobalStyles.colors.primary100,
+        padding: 6,
+        borderRadius: 6,
+        fontSize: 18,
+        color: GlobalStyles.colors.primary700
+    },
+    inputMultiLine: {
+        minHeight: 100,
+        textAlignVertical: 'top'
+    }
+})
