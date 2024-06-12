5f66b23d4dab58e919225b1481112ef6ffe5ba9d
ratanlalmourya <ratanlalmourya41@gmail.com>
2024-06-12 09:08:15 +0530

    Formatting the dates

--- expense-tracker-app/components/expenses_output/ExpenseItem.js
+++ expense-tracker-app/components/expenses_output/ExpenseItem.js
@@ -1,5 +1,6 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
+import { getFormattedDate } from "../../util/date";
 
 function ExpenseItem({description,amount,date}) {
   return (
@@ -7,7 +8,7 @@ function ExpenseItem({description,amount,date}) {
       <View style={styles.expenseitem}>
         <View>
           <Text style={[styles.textBase,styles.description]}>{description}</Text>
-          <Text style={styles.textBase}>2021-20-10</Text>
+          <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
         </View>
         <View style={styles.amountContainer}>
           <Text style={styles.amount}>{amount}</Text>
@@ -47,7 +48,8 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 4
+        borderRadius: 4,
+        minWidth: 80
     },
     amount: {
         color: GlobalStyles.colors.primary500,
