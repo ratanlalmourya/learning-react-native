4ac0120b9a6532226dfd84dd98ce70792d215bc0
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-06-19 12:25:43 +0000

    Logging Users In

--- user-authentication-project/util/auth.js
+++ user-authentication-project/util/auth.js
@@ -1,14 +1,21 @@
 import axios from 'axios';
 
 const API_KEY = 'AIzaSyDX_CxL_0uRSfMkA9Kzwn4rNVWJa1B0M6Y'
-export async function createUser(email,password) {
-    const response = await axios.post(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
-        {
+
+export async function authenticate(mode,email,password) {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+    const response = await axios.post(url, {
             email: email,
             password: password,
             returnSecureToken: true
-        }
-    );
+        });
+    console.log(response.data);
+}
+export async function createUser(email,password) {
+    await authenticate("signUp",email,password);
+}
+
+export async function login(email,password) {
+    await authenticate("signInWithPassword",email,password);
 }
 
