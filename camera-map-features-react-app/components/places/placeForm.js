f427b77337228d792ef1adf4074f0a9ffe0ee0e6
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-06-22 09:57:25 +0000

    screens , components and models created for project

---
+++ camera-map-features-react-app/components/places/placeForm.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+
+function PlaceForm() {
+
+    return (
+        <View>
+            <Text>The Place Form</Text>
+        </View>
+    )
+}
+
+
+export default PlaceForm;
