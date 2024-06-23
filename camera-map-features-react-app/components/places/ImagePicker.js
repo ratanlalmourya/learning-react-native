ca1ca5583ef0487d8e98f54c181ba7971936a816
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-06-23 12:00:51 +0000

    taking photos on camera and managing permission

--- camera-map-features-react-app/components/places/ImagePicker.js
+++ camera-map-features-react-app/components/places/ImagePicker.js
@@ -1,9 +1,33 @@
-import { Button, View } from "react-native";
-import { launchCameraAsync } from 'expo-image-picker';
+import { Alert, Button, View } from "react-native";
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
 
-function ImagePicker() {    
+function ImagePicker() {        
 
-    async function takeImageHandler() {   
+    const [cameraPermissionInformation,requestPermisson] = useCameraPermissions();
+
+    async function verifyPermissions() {
+        if(cameraPermissionInformation.status === PermissionStatus.UNDETERMINED ){
+            const permissonResponse = await requestPermisson();
+            return permissonResponse.granted;
+        }
+
+        if(cameraPermissionInformation.status === PermissionStatus.DENIED ){
+            Alert.alert(
+                'Insufficient Permissons!',
+                'You need to grant  camera permissons to use this app.'
+            )
+
+            return false;
+        }
+
+        return true;
+    }
+
+    async function takeImageHandler() { 
+        const hasPermission = await verifyPermissions();    
+        if(!hasPermission) {
+            return;
+        }   
        const image =  await launchCameraAsync({
         allowsEditing: true,
         aspect: [19,9],
