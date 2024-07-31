0b308c479b54ef9162c11b90ef50081f166e5b14
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-07-31 15:26:59 +0000

    Send and receiving notificatino using expo-notifcation

--- pushNotificationApp/App.js
+++ pushNotificationApp/App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+      if (status !== 'granted') {
+        await Notifications.requestPermissionsAsync();
+      }
+    })();
+  }, []);
+
+  async function scheduleNotification() {
+    console.log("Push Notification Called");
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'My First Location Notification',
+        body: 'This is the body of the notification. Ratan Mourya',
+        data: { userName: 'Ratan' },
+      },
+      trigger: { seconds: 2 },
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Schedule Notification" onPress={scheduleNotification} />
       <StatusBar style="auto" />
     </View>
   );
