0f264201478c0e675d77d4b6de0999d6c95bce80
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-08-05 12:09:46 +0000

    Sending push notification

--- pushNotificationApp/App.js
+++ pushNotificationApp/App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { Alert, Button, Platform, StyleSheet, View } from 'react-native';
 import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -12,6 +13,38 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() { 
+
+  useEffect(() => { 
+    
+    (async () => {
+      const { status } = await Notifications.getPermissionsAsync();
+      if (status !== 'granted') {
+        await Notifications.requestPermissionsAsync();
+      }
+
+      if(status !== 'granted') {
+        Alert.alert(
+          'Permission required',
+          'Push notification need the appropriate permissons.'
+        );
+        return;
+      }
+      const projectId = Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+      await Notifications.getExpoPushTokenAsync({ projectId: '54640c8b-cfff-4638-a85b-12a5dc85d0de'}).then((pushTokenData) => {
+        console.log("PushToekn",pushTokenData);
+      }).catch((error) => {
+        console.log("PushToekn Did not rceived ",error);
+      });
+     
+      if(Platform.OS === 'android') {
+        await Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.HIGH
+        });
+      }
+
+    })();
+  },[]);
 
   useEffect(() => { 
     const subscription1 = Notifications.addNotificationReceivedListener((notification) => {
@@ -25,13 +58,6 @@ export default function App() {
       console.log(response);
     });
     
-    (async () => {
-      const { status } = await Notifications.getPermissionsAsync();
-      if (status !== 'granted') {
-        await Notifications.requestPermissionsAsync();
-      }
-    })();
-
     return () => {
       subscription1.remove();
       subscription2.remove();
